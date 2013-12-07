14ae82ce56e9eca6a89dcee92491adca6d5df0d2
takuya4880 <takuya4880@gmail.com>
2013-12-08 00:46:24 +0900

    debug

--- detdt.f90
+++ detdt.f90
@@ -17,7 +17,7 @@ subroutine detdt(box)
 
     !$omp parallel workshare
     v2 = (box%rovx**2 + box%rovy**2 + box%rovz**2)/(box%ro**2)
-    v2 = v2 + (box%bx**2 + box%bz**2 + box%bz**2)/box%ro
+    v2 = v2 + (box%bx**2 + box%by**2 + box%bz**2)/box%ro
     v2 = v2 + box%con%gam * box%pr / box%ro
     !$omp end parallel workshare
 
