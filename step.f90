26625749bab6c8e8aebfeaf1e0d79ea637710b9d
takuya4880 <takuya4880@gmail.com>
2013-12-07 19:18:30 +0900

    3D!

--- step.f90
+++ step.f90
@@ -8,34 +8,34 @@ subroutine step(box)
 
     implicit none
     type(cell) :: box
-    type(cell), pointer :: fx, fz, s, h, d
+    type(cell), pointer :: fx, fy, fz, s, h, d
     integer :: i
     allocate(fx, fz, s, h, d)
     fx = box
+    fy = box
     fz = box
     s = box
     h = box
-    h%con%a = -1.   !use a to knowtify this is half step value for source term
     d = box
+    h%con%a = -1.   !use a to knowtify this is half step value for source term
         
-    !forall(i=1:ix) h%x(i)=h%con%dx*(i-h%con%marg-1.)
-    !forall(i=1:iz) h%z(i)=h%con%dz*(i-h%con%marg-1.)
     h%x = box%x + 0.5*box%con%dx
+    h%y = box%x + 0.5*box%con%dx
     h%z = box%z + 0.5*box%con%dz
 
-    call flux(box, fx, fz)
+    call flux(box, fx, fy, fz)
     call source(box, s)
-    call lw1(box, h, d, fx, fz, s)
+    call lw1(box, h, d, fx, fy, fz, s)
     call pressure(h)
     
-    call flux(h, fx, fz)
+    call flux(h, fx, fy, fz)
     call source(h, s)
-    call lw2(box, d, fx, fz, s)
+    call lw2(box, d, fx, fy, fz, s)
 
     call artvis(box, d)
     call pressure(box)
 
-    deallocate(fx, fz, s, h, d)
+    deallocate(fx, fy, fz, s, h, d)
 
 end subroutine
 end module
