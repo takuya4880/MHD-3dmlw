09ac96bc3fef347ee371fdd7a2b5f829c597bb2e
takuya4880 <takuya4880@gmail.com>
2013-12-12 23:35:25 +0900

    implementation of 3d output

--- output.f90
+++ output.f90
@@ -9,14 +9,14 @@ subroutine outp(box,t)
     double precision :: t
 
     write(box%op%mf_t) t
-    write(box%op%mf_ro) box%ro(:,10,:)
-    write(box%op%mf_pr) box%pr(:,10,:)
-    write(box%op%mf_vx) box%rovx(:,10,:)/box%ro(:,10,:)
-    write(box%op%mf_vy) box%rovz(:,10,:)/box%ro(:,10,:)
-    write(box%op%mf_vz) box%rovz(:,10,:)/box%ro(:,10,:)
-    write(box%op%mf_bx) box%bx(:,10,:)
-    write(box%op%mf_by) box%bz(:,10,:)
-    write(box%op%mf_bz) box%bz(:,10,:)
+    write(box%op%mf_ro) box%ro
+    write(box%op%mf_pr) box%pr
+    write(box%op%mf_vx) box%rovx/box%ro
+    write(box%op%mf_vy) box%rovy/box%ro
+    write(box%op%mf_vz) box%rovz/box%ro
+    write(box%op%mf_bx) box%bx
+    write(box%op%mf_by) box%by
+    write(box%op%mf_bz) box%bz
 
 end subroutine 
 
@@ -28,13 +28,14 @@ subroutine outpinit(box)
     
     integer :: mpe
     character :: cno*4
-    mpe = (box%con%imz-1)*cox + box%con%imx -1
+    mpe = this_image()-1
     write(cno,'(i4.4)') mpe
 
     box%op%mf_params=9
     box%op%mf_t=10
     box%op%mf_x=11
     box%op%mf_y=12
+    box%op%mf_z=13
     box%op%mf_ro=20
     box%op%mf_pr=21
     box%op%mf_vx=22
@@ -46,34 +47,40 @@ subroutine outpinit(box)
     
     call dacdefparam(box%op%mf_params,'params.txt.'//cno)
     call dacdef0s(box%op%mf_t,'t.dac.'//cno,6)
-    call dacdef2s(box%op%mf_ro,'ro.dac.'//cno,6,ix,iz)
-    call dacdef2s(box%op%mf_pr,'pr.dac.'//cno,6,ix,iz)
-    call dacdef2s(box%op%mf_vx,'vx.dac.'//cno,6,ix,iz)
-    call dacdef2s(box%op%mf_vy,'vy.dac.'//cno,6,ix,iz)
-    call dacdef2s(box%op%mf_vz,'vz.dac.'//cno,6,ix,iz)
-    call dacdef2s(box%op%mf_bx,'bx.dac.'//cno,6,ix,iz)
-    call dacdef2s(box%op%mf_by,'by.dac.'//cno,6,ix,iz)
+    call dacdef3s(box%op%mf_ro,'ro.dac.'//cno,6,ix,iy,iz)
+    call dacdef3s(box%op%mf_pr,'pr.dac.'//cno,6,ix,iy,iz)
+    call dacdef3s(box%op%mf_vx,'vx.dac.'//cno,6,ix,iy,iz)
+    call dacdef3s(box%op%mf_vy,'vy.dac.'//cno,6,ix,iy,iz)
+    call dacdef3s(box%op%mf_vz,'vz.dac.'//cno,6,ix,iy,iz)
+    call dacdef3s(box%op%mf_bx,'bx.dac.'//cno,6,ix,iy,iz)
+    call dacdef3s(box%op%mf_by,'by.dac.'//cno,6,ix,iy,iz)
 
     call dacputparami(box%op%mf_params,'ix',ix)
-    call dacputparami(box%op%mf_params,'jx',iz)
+    call dacputparami(box%op%mf_params,'jx',iy)
+    call dacputparami(box%op%mf_params,'kx',iz)
     call dacputparami(box%op%mf_params,'margin',marg)
     call dacputparami(box%op%mf_params,'mpi',1)
-    call dacputparami(box%op%mf_params,'mpex',cox*coz)
+    call dacputparami(box%op%mf_params,'mpex',cox*coy*coz)
     call dacputparami(box%op%mf_params,'mpe',mpe)
     call dacputparami(box%op%mf_params,'ipex',cox)
     call dacputparami(box%op%mf_params,'ipe',box%con%imx-1)
-    call dacputparami(box%op%mf_params,'jpex',coz)
-    call dacputparami(box%op%mf_params,'jpe',box%con%imz-1)
+    call dacputparami(box%op%mf_params,'jpex',coy)
+    call dacputparami(box%op%mf_params,'jpe',box%con%imy-1)
+    call dacputparami(box%op%mf_params,'kpex',coz)
+    call dacputparami(box%op%mf_params,'kpe',box%con%imz-1)
     
 
     call dacdef1d(box%op%mf_x,'x.dac.'//cno,6,ix)
     write(box%op%mf_x) box%x
-    call dacdef1d(box%op%mf_y,'y.dac.'//cno,6,iz)
+    call dacdef1d(box%op%mf_y,'y.dac.'//cno,6,iy)
+    write(box%op%mf_y) box%y
+    call dacdef1d(box%op%mf_z,'y.dac.'//cno,6,iz)
     write(box%op%mf_y) box%z
     call dacputparamd(box%op%mf_params,'gm',box%con%gam)
 
     close(box%op%mf_x)
     close(box%op%mf_y)
+    close(box%op%mf_z)
     close(box%op%mf_params)
      
 end subroutine
@@ -85,10 +92,10 @@ subroutine readdata(box,t)
     type (cell) :: box
     double precision :: t
 
-    integer :: ndi,n,nx0,ix0,jx0,mtype
+    integer :: ndi,n,nx0,ix0,jx0,kx0,mtype
     integer :: mpe
     character :: cno*4
-    mpe = (box%con%imz-1)*cox + box%con%imx -1
+    mpe = this_image() -1
     write(cno,'(i4.4)') mpe
 
     ndi=1000
@@ -104,14 +111,14 @@ subroutine readdata(box,t)
     box%op%mfi_by=77
     
     call dacopnr0s(box%op%mfi_t,'in/t.dac.'//cno,mtype,nx0)
-    call dacopnr2s(box%op%mfi_ro,'in/ro.dac.'//cno,mtype,ix0,jx0,nx0)
-    call dacopnr2s(box%op%mfi_pr,'in/pr.dac.'//cno,mtype,ix0,jx0,nx0)
-    call dacopnr2s(box%op%mfi_vx,'in/vx.dac.'//cno,mtype,ix0,jx0,nx0)
-    call dacopnr2s(box%op%mfi_vy,'in/vy.dac.'//cno,mtype,ix0,jx0,nx0)
-    call dacopnr2s(box%op%mfi_vz,'in/vz.dac.'//cno,mtype,ix0,jx0,nx0)
-    call dacopnr2s(box%op%mfi_bx,'in/bx.dac.'//cno,mtype,ix0,jx0,nx0)
-    call dacopnr2s(box%op%mfi_by,'in/by.dac.'//cno,mtype,ix0,jx0,nx0)
-    call dacopnr2s(box%op%mfi_bz,'in/bz.dac.'//cno,mtype,ix0,jx0,nx0)
+    call dacopnr3s(box%op%mfi_ro,'in/ro.dac.'//cno,mtype,ix0,jx0,kx0,nx0)
+    call dacopnr3s(box%op%mfi_pr,'in/pr.dac.'//cno,mtype,ix0,jx0,kx0,nx0)
+    call dacopnr3s(box%op%mfi_vx,'in/vx.dac.'//cno,mtype,ix0,jx0,kx0,nx0)
+    call dacopnr3s(box%op%mfi_vy,'in/vy.dac.'//cno,mtype,ix0,jx0,kx0,nx0)
+    call dacopnr3s(box%op%mfi_vz,'in/vz.dac.'//cno,mtype,ix0,jx0,kx0,nx0)
+    call dacopnr3s(box%op%mfi_bx,'in/bx.dac.'//cno,mtype,ix0,jx0,kx0,nx0)
+    call dacopnr3s(box%op%mfi_by,'in/by.dac.'//cno,mtype,ix0,jx0,kx0,nx0)
+    call dacopnr3s(box%op%mfi_bz,'in/bz.dac.'//cno,mtype,ix0,jx0,kx0,nx0)
 
     do n=1,ndi
         read(box%op%mfi_t,end=9900) t
